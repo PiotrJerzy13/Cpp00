4b92eefd9675b11a45df1da161d114115d9fc91b
Piotr Wojnarowski <pwojnaro@1-C-31.42heilbronn.de>
2024-11-01 22:36:08 +0100

    First

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pwojnaro <pwojnaro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 22:03:51 by pwojnaro          #+#    #+#             */
+/*   Updated: 2024/11/01 22:30:20 by pwojnaro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+#include <string>
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+    }
+    else
+    {
+		for (int i = 1; i < argc; ++i)
+		{
+			std::string arg = argv[i];
+        	for (int j = 0; j < arg.size(); ++j)
+       		{
+                std::cout << (char)(std::toupper(arg[j]));
+        	}
+        	std::cout << "\n";
+		}
+    }
+    return 0;
+}
